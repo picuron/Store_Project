3964c755e126356e34f0dd96bb5de9b9464f8b2c
Eric Herscovich <eric.herscovich@shopify.com>
2020-11-28 19:55:38 -0500

    Created file structure, added directory initalization

---
+++ Store_Project/src/store/DirectoryInitilization.java
@@ -0,0 +1,39 @@
+package store;
+
+import javax.xml.crypto.Data;
+import java.io.File;
+
+public class DirectoryInitilization {
+    public static void Setup(){
+
+        //Create Data directory, and directories for Inventory Data, Order Data and Customer Data
+        File DataDir = new File("Data");
+        File InventoryDir = new File("Data/Inventory");
+        File OrderDir = new File("Data/Order");
+        File CustomerDir = new File("Data/Customer");
+
+        //If the /Data path doesn't exist, then make it, and add the other subdirectories
+        if (!DataDir.exists()){
+            if(DataDir.mkdir()){
+                InventoryDir.mkdir();
+                OrderDir.mkdir();
+                CustomerDir.mkdir();
+            }
+            else{
+                System.out.println("Directory failed to be made.");
+            }
+        }
+
+        if(!InventoryDir.exists()){
+            InventoryDir.mkdir();
+        }
+
+        if(!OrderDir.exists()){
+            OrderDir.mkdir();
+        }
+
+        if(!CustomerDir.exists()){
+            CustomerDir.mkdir();
+        }
+    }
+}
--- Store_Project/src/store/StoreApplication.java
+++ Store_Project/src/store/StoreApplication.java
@@ -1,4 +1,8 @@
 package store;
 
 public class StoreApplication {
+
+    public static void main(String[] args){
+        DirectoryInitilization.Setup();
+    }
 }

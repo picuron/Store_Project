fd3dc5161ec8d34757478993f574a0443f306beb
Eric Herscovich <eric.herscovich@shopify.com>
2020-11-30 20:11:32 -0500

    Added customer class

---
+++ Store_Project/src/store/Customer.java
@@ -0,0 +1,47 @@
+package store;
+
+public class Customer {
+    private String name;
+    private String email;
+    private String address;
+    private int creditCardNumber;
+
+    public Customer(String name, String email, String address, int creditCardNumber) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(int creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+}
